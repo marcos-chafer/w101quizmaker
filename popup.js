befd42a1b6049cfec0639e4d1631264c76820fcd
hori-hrzn <horihrzn@gmail.com>
2023-11-25 11:49:54 +0100

    Removed start button from popup

--- popup.js
+++ popup.js
@@ -1,17 +1,4 @@
-document.addEventListener("DOMContentLoaded",()=>{
-
-	const startButtonPopup = document.getElementById("startButtonPopup");
-
-	startButtonPopup.addEventListener('click',() => {
-		// if(!confirm("W101 Trivia Maker is going to start completing quizs for you. Are you signed up?")) return;
-		
-		// chrome.runtime.sendMessage({action:"goToQuiz0"});
-
-		chrome.runtime.sendMessage({action: "answerQuiz"});
-	
-	})
-	
-	
+document.addEventListener("DOMContentLoaded",()=>{	
 	
 
 })
